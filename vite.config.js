0f219606f8bcf7294ceb5e356b291919c9f79427
vincy <vnceer@gmail.com>
2026-02-14 09:53:42 +0530

    error 404

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Product-listing-using-React/',
+  base: '/productlist/',
   plugins: [react(),tailwindcss()],
 })
